922e059dfd9e0a9721a1d045723dc50e213b7eb8
M <beam2675@usherbrooke.ca>
2022-11-19 14:55:53 -0500

    Setup project, cleared old folders

--- Django_ws/Autoprint_site/interface/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='AutoPrint-home'),
-    path('interaction/', views.interaction, name='interaction'),
-    path('buttons/', views.buttons, name='buttons'),
-    path('printer_status', views.Printer_status, name='Printer_status')
-]
